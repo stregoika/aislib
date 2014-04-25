3cfbee02e7656d87abb11721f742b111e4cc03ed
sysuser <sysuser@tardis.local>
2014-04-25 10:29:45 +0200

    18

--- ais/ais_msg_18.py
+++ ais/ais_msg_18.py
@@ -1331,8 +1331,8 @@ def main():
 		}
 
 		bits = encode(msgDict)
-		if 'binary'==options.ioType: print str(bits)
-		elif 'nmeapayload'==options.ioType:
+		if 'binary' == options.ioType: print str(bits)
+		elif 'nmeapayload' == options.ioType:
          # FIX: figure out if this might be necessary at compile time
          #print "bitLen",len(bits)
          bitLen=len(bits)
@@ -1340,18 +1340,14 @@ def main():
             bits = bits + BitVector(size=(6 - (bitLen%6)))  # Pad out to multiple of 6
          #print "result:",binary.bitvectoais6(bits)[0]
          print binary.bitvectoais6(bits)[0]
-
-
 		# FIX: Do not emit this option for the binary message payloads.  Does not make sense.
 		elif 'nmea'==options.ioType: 
          #bitLen=len(bits)
-                    #if bitLen%6!=0:
+         #if bitLen%6!=0:
          #	bits = bits + BitVector(size=(6 - (bitLen%6)))  # Pad out to multiple of 6
          import aisutils.uscg as uscg
          nmea = uscg.create_nmea(bits)
          print nmea
-         #
-         #
 
 
          #sys.exit("FIX: need to implement creating nmea capability")
