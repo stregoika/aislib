c4aa135c953e298841707bc6bfe362072f8ee0b1
Sonia Gómara <sonia@artax.local>
2014-05-02 12:48:05 +0200

    add ddbb

---
+++ ddbb_utils/ce_fisheries-clean_vessel.sql
@@ -0,0 +1,73 @@
+﻿CREATE OR REPLACE FUNCTION ce_fisheries.clean_vessel()
+RETURNS void AS $$
+BEGIN
+   INSERT INTO ce_fisheries.vessel 
+   (
+     	id,
+   	country_code,
+	cfr,
+            active, 
+   	reg_num,
+   	ext_marking,
+   	vessel_name,
+   	port_code,
+   	port_name,
+   	ircs_code,
+   	ircs, 
+   	vms_code, 
+   	gear_main_code, 
+   	gear_sec_code,
+   	loa, 
+   	lbp,
+   	ton_ref, 
+   	ton_gt,
+   	ton_oth,
+   	ton_gts,
+   	power_main,
+   	power_aux,
+   	hull_material,
+   	com_year,
+   	com_month,
+   	com_day,
+   	segment , 
+  	construction_year,
+  	construction_place, 
+	import_date
+   )
+SELECT DISTINCT ON (cfr)
+(
+            id, 
+            country_code, 
+   	cfr,
+           ‘Y’,
+   	reg_num,
+   	ext_marking,
+   	vessel_name,
+   	port_code,
+   	port_name,
+   	ircs_code,
+   	ircs, 
+   	vms_code, 
+   	gear_main_code, 
+   	gear_sec_code,
+   	loa, 
+   	lbp,
+   	ton_ref, 
+   	ton_gt,
+   	ton_oth,
+   	ton_gts,
+   	power_main,
+   	power_aux,
+   	hull_material,
+   	com_year,
+   	com_month,
+   	com_day,
+   	segment , 
+  	construction_year,
+  	construction_place, 
+	import_date
+)
+FROM ce_fisheries.vessel_raw
+ORDER BY cfr, import_date DESC, event_end_date DESC;
+END;
+LANGUAGE plpqsql;
