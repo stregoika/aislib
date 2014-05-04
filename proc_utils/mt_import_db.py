cae87a428c6839a240203cdb18b35c14af9f23ab
stregoika <sonia.gomara@gmail.com>
2014-05-04 23:56:18 +0200

    mt import

--- proc_utils/mt_import_db.py
+++ proc_utils/mt_import_db.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
-import time
+#import time
+import datetime
 import os.path
 import socket
+import psycopg2
 
 # Variables de sistema
 APP_PATH='/home/sysuser/proc_utils/'
@@ -10,14 +12,22 @@ BASE_PATH='/home/jllodra/workspace/aisHijack/csv/'
 LOG=APP_PATH + "logs/mt_import_db.log"
 LOCK= APP_PATH + "mt_import_db.lock"
 
-#print "app_path: {}".format(APP_PATH)
-
 # Fecha
-tiempo=time.localtime()
-fecha=time.strftime("%Y-%m-%d_%H:%M:%S",tiempo)
+date = datetime.datetime.now()
+fecha = date.strftime("%Y-%m-%d_%H:%M:%S")
 
+# Fichero log
 file_log = open(LOG,'w+')
 file_log.write("-+- START "+fecha+"-+-\n")
+
+#Cabecera tabla
+# id | name | mmsi | ship_cargo_name | sog | cog | latitude | longitude | status | date_time 
+campos = "(name, mmsi, ship_cargo_name, sog, cog, latitude, longitude, status, date_time)"
+tabla = "marine_traffic.ais_raw"
+
+#Conexion ddbb
+#connectStr = "dbname='"+options.databaseName+"' user='"+options.databaseUser+"' host='"+options.databaseHost+"'"
+opciones_db = " dbname=ais user=postgres password=+mngp0st+ port=5433 host=scb-dattest"
 
 # Check if it is already running
 if os.path.isfile(LOCK):
@@ -26,53 +36,36 @@ if os.path.isfile(LOCK):
 else:
     file_lock = open(LOCK,'w+')
     file_lock.write("$$ "+socket.gethostname())
-    file_log.write("$$ "+socket.gethostname())
 
-    year=time.strftime("%Y",tiempo)
-#fn = '/home/helmi/world.csv'
-#columns = file(fn).readline()
-#print 'create table world (%s text);' % " text,".join(columns.split(','))
-#print "copy world from '%s' with csv header;" % fn
+    date_ayer = date - datetime.timedelta(days=1)
+    fecha_ayer = date_ayer.strftime("%Y-%m-%d_%H:%M:%S")
+    year = date_ayer.year
+    month = date_ayer.month
+    day = date_ayer.day
+    print "date: {}".format(str(date))
+    print "fecha: {}".format(str(fecha))
+    print "ayer: {}".format(str(date_ayer))
+    print "fecha2: {}".format(str(fecha_ayer))
+    print "anyo: {} mes: {}  dia: {}".format(str(year),str(month), str(day))
 
-#base_path='/home/jllodra/workspace/aisHijack/csv/'
+    # Recorrer el directorio base buscando los ficheros *.csv
+    for root, dirnames, filenames in os.walk(BASE_PATH):
+        for filename in filenames:
+            if filename.endswith('.csv'):
+               print "fichero: {}".format(str(filename))
+               sentencia="COPY "+tabla+" "+campos+" FROM '"+root+"/"+filename+"' WITH DELIMITER ';' CSV HEADER;"
+               print sentencia
+	       conexion = psycopg2.connect(opciones_db)
+               cursor_con = conexion.cursor()
+               cursor_con.execute(sentencia)
+               conexion.commit()
+        break # para evitar que profundice en los directorios
 
-#archivar los ficheros del dia anterior
-#year=$(date -d "-1 day" +%Y)
-#month=$(date -d "-1 day" +%m)
-#day=$(date -d "-1 day" +%d)
+    cursor_con.close()
+    conexion.close()
 
-#echo "anyo: "$year
-#echo "mes: "$month
-#echo "dia: "$day
-
-#archive_path=${base_path}'/'${year}'/'${month}
-
-#ais_marinetraffic_2014-01-21_16:10:01_UTC.kml.csv
-#files_to_be_archive=`find ${base_path} -maxdepth 1 -name "ais_marinetraffic_${year}-${month}-${day}_*.kml.csv"`
-#echo "ficheros: " $files_to_be_archive
-
-#number_of_chars=`echo $files_to_be_archive | wc -c`
-        
-#if [ $number_of_chars -gt 1 ]; then
-            
-	#Create the archive path if not exists
-#	mkdir -p ${archive_path}
-        # Move each file to the archive path
-#	mv -f ${files_to_be_archive} ${archive_path}
-#
-#        echo "Files archived to "${archive_path} >> $LOG
-#
-#else
-
-#	echo "There aren't new file inside the input directory "${base_path} >> $LOG
-
-#fi
-
-# Remove lock file
-#rm -f "$LOCK" &> /dev/null
-
-#echo "-+- END: $(date) -+-" >> "$LOG"
     file_lock.close()
     os.remove(LOCK)
 
+file_log.write("-+- END "+fecha+"-+-\n")
 file_log.close()
